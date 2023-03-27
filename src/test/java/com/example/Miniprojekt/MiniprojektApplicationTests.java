b2fc68ac93cb889ddf6592056fe9181d3ac8fc81
Andsemandse <andr90m3@stud.kea.dk>
2023-03-27 09:38:32 +0200

    God start

---
+++ src/test/java/com/example/Miniprojekt/MiniprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Miniprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
