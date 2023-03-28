b3df9f1d22e72880db543746689c686447eb466a
YmerA38 <stin166c@stud.kea.dk>
2023-03-28 12:30:10 +0200

    with list in html

--- src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
+++ src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
@@ -2,6 +2,7 @@ package com.example.Miniprojekt.Repository;
 
 import com.example.Miniprojekt.Model.Wish;
 import com.example.Miniprojekt.Utility.ConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -20,21 +21,22 @@ public class WishListRepository implements InterfaceRepository {
         ArrayList<Wish> resultList = new ArrayList<>();
         try {
             Connection con = ConnectionManager.getConnection(url,user,psw);
-            String SQL = "select * from superheroesdata.superhero";
+            String SQL = "select * from wishlist_DB.wich";
             ResultSet rs = con.createStatement().executeQuery(SQL);
 
             while (rs.next()) {
                 Wish wish = new Wish();
-
+                wish.setWishName(rs.getString("wishName"));
+                //wish.setAmount(rs.getInt("amount"));
+                wish.setDescription(rs.getString("description"));
+                wish.setLink(rs.getString("link"));
+                wish.setFulfilled(rs.getBoolean("fulfilled"));
+                wish.setPrice(rs.getInt("price"));
                 resultList.add(wish);
             }
         }catch (SQLException e){
             throw new RuntimeException();
         }
-
-
-
-
 
         return resultList;
     }
