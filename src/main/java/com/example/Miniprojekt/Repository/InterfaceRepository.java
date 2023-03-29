7579a8ea2f96ce084ff4b6f919802a103c399221
YmerA38 <stin166c@stud.kea.dk>
2023-03-29 11:39:18 +0200

    small changes

--- src/main/java/com/example/Miniprojekt/Repository/InterfaceRepository.java
+++ src/main/java/com/example/Miniprojekt/Repository/InterfaceRepository.java
@@ -1,9 +1,15 @@
 package com.example.Miniprojekt.Repository;
 
+import com.example.Miniprojekt.Model.Users;
 import com.example.Miniprojekt.Model.Wish;
+import com.example.Miniprojekt.Model.Wishlist;
 
 import java.util.ArrayList;
 
 public interface InterfaceRepository {
     ArrayList<Wish> getWishList(int id);
+
+    ArrayList<Wishlist> getListOfLists(int id);
+
+    ArrayList<Users> getListOfUsers();
 }
