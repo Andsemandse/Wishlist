f9512e5147b09756c3581ba67b4c74bae5d7251e
Andsemandse <andr90m3@stud.kea.dk>
2023-03-29 13:04:50 +0200

    AddWish

--- src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
+++ src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
@@ -20,10 +20,10 @@ public class WishListRepository implements InterfaceRepository {
     @Value("${spring.utility.psw}")
     String psw;
 
-    public ArrayList<Users> getListOfUsers(){
+    public ArrayList<Users> getListOfUsers() {
         ArrayList<Users> resultList = new ArrayList<>();
         try {
-            Connection con = ConnectionManager.getConnection(url,user,psw);
+            Connection con = ConnectionManager.getConnection(url, user, psw);
             String SQL = "select * from wishlist_DB.users";
             ResultSet rs = con.createStatement().executeQuery(SQL);
 
@@ -32,7 +32,7 @@ public class WishListRepository implements InterfaceRepository {
                 //user.setUserName(rs.getString("title"));
                 resultList.add(user);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException();
         }
 
@@ -40,13 +40,13 @@ public class WishListRepository implements InterfaceRepository {
     }
 
 
-    public ArrayList<Wishlist> getListOfWishlists(int id){
+    public ArrayList<Wishlist> getListOfWishlists(int id) {
         ArrayList<Wishlist> resultList = new ArrayList<>();
         try {
-            Connection con = ConnectionManager.getConnection(url,user,psw);
+            Connection con = ConnectionManager.getConnection(url, user, psw);
             String SQL = "select * from wishlist_DB.wishlist where user_id = ?";
             PreparedStatement ps = con.prepareStatement(SQL);
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
@@ -54,66 +54,69 @@ public class WishListRepository implements InterfaceRepository {
                 //wishlist.setTitle(rs.getString("title"));
                 resultList.add(wishlist);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException();
         }
 
         return resultList;
     }
-    public ArrayList<Wish> getWishList(int id){
+
+    public ArrayList<Wish> getWishList(int id) {
         ArrayList<Wish> resultList = new ArrayList<>();
         try {
-            Connection con = ConnectionManager.getConnection(url,user,psw);
+            Connection con = ConnectionManager.getConnection(url, user, psw);
             String SQL = "select * from wishlist_DB.wish where wishlist_id = ?";
             PreparedStatement ps = con.prepareStatement(SQL);
-            ps.setInt(1,id);
+            ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
                 Wish wish = new Wish();
                 wish.setWishName(rs.getString("wishName"));
                 //wish.setAmount(rs.getInt("amount"));
-                wish.setDescription(rs.getString("details"));
+                wish.setDetails(rs.getString("details"));
                 wish.setLink(rs.getString("link"));
                 wish.setFulfilled(rs.getBoolean("fulfilled"));
                 wish.setPrice(rs.getInt("price"));
                 resultList.add(wish);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException();
         }
 
         return resultList;
     }
 
-    //insert wish into wish table
-    public void addWish(WishListFormDTO form) {
+
+    public void addWish(Wish form) {
         try (Connection con = DriverManager.getConnection(url, user, psw)) {
 
-
-            String insertWishSQL = "INSERT INTO wish (wishname, details, price) VALUES (?, ?, ?)";
+            String insertWishSQL = "INSERT INTO wish (wishname, details, price, amount) VALUES (?, ?, ?, ?)";
             PreparedStatement insertWishStmt = con.prepareStatement(insertWishSQL, Statement.RETURN_GENERATED_KEYS);
-            insertWishStmt.setString(1, "wishName");
-            insertWishStmt.setString(2, "details");
-            insertWishStmt.setInt(3, Integer.parseInt("price"));
+            insertWishStmt.setString(1, form.getWishName());
+            insertWishStmt.setString(2, form.getDetails());
+            insertWishStmt.setInt(3, form.getPrice());
+            insertWishStmt.setInt(4, form.getAmount());
+
             insertWishStmt.executeUpdate();
 
             // get generated wish ID
             ResultSet rs = insertWishStmt.getGeneratedKeys();
-            rs.next();
-            int wishId = rs.getInt(1);
+            if (rs.next()) {
+                int wishId = rs.getInt(1);
+                int UserId = rs.getInt(2);
 
-
-            //insert wishlist item into wishlist table
-            String insertWishlistSQL = "INSERT INTO wishlist (wish_id, user_id, amount) VALUES (?, ?, ?)";
-            PreparedStatement insertWishlistStmt = con.prepareStatement(insertWishlistSQL); insertWishlistStmt.setInt(1, wishId);
-            insertWishlistStmt.setInt(2, Integer.parseInt("userId"));
-            insertWishlistStmt.setInt(3, Integer.parseInt("amount"));
-            insertWishlistStmt.executeUpdate();
-
+                //insert wishlist item into wishlist table
+                String insertWishlistSQL = "INSERT INTO wishlist (wish_id, user_id, amount) VALUES (?, ?)";
+                PreparedStatement insertWishlistStmt = con.prepareStatement(insertWishlistSQL);
+                insertWishlistStmt.setInt(1, wishId);
+                insertWishlistStmt.setInt(2, UserId);
+                insertWishlistStmt.executeUpdate();
+            }
 
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
         }
     }
+
 }
