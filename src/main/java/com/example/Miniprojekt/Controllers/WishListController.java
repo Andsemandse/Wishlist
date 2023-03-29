47af6262fa14bec338aa140634623b5987f45681
YmerA38 <stin166c@stud.kea.dk>
2023-03-29 09:23:48 +0200

    small changes

--- src/main/java/com/example/Miniprojekt/Controllers/WishListController.java
+++ src/main/java/com/example/Miniprojekt/Controllers/WishListController.java
@@ -6,6 +6,7 @@ import com.example.Miniprojekt.Repository.WishListRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("wishlist")
@@ -14,9 +15,9 @@ public class WishListController {
 
     InterfaceRepository repository = new WishListRepository();
 
-    @GetMapping("show")
-    public String showWishes(Model model){
-        model.addAttribute("list",repository.getList());
+    @GetMapping("showlist/{id}")
+    public String showWishes(Model model, @PathVariable int id){
+        model.addAttribute("list",repository.getWishList(id));
 
         return "show_list";
     }
--- src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
+++ src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
@@ -3,32 +3,38 @@ package com.example.Miniprojekt.Repository;
 import com.example.Miniprojekt.Model.Wish;
 import com.example.Miniprojekt.Utility.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
+@Component
 public class WishListRepository implements InterfaceRepository {
 
+    @Value("${spring.utility.url}")
     String url;
-
+    @Value("${spring.utility.user}")
     String user;
-
+    @Value("${spring.utility.psw}")
     String psw;
 
-    public ArrayList<Wish> getList(){
+    // TODO: 29-03-2023 metode skal rettes.
+    public ArrayList<Wish> getWishList(int id){
         ArrayList<Wish> resultList = new ArrayList<>();
         try {
             Connection con = ConnectionManager.getConnection(url,user,psw);
-            String SQL = "select * from wishlist_DB.wich";
-            ResultSet rs = con.createStatement().executeQuery(SQL);
+            String SQL = "select * from wishlist_DB.wish where wishlist_id = ?";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
 
             while (rs.next()) {
                 Wish wish = new Wish();
                 wish.setWishName(rs.getString("wishName"));
                 //wish.setAmount(rs.getInt("amount"));
-                wish.setDescription(rs.getString("description"));
+                wish.setDescription(rs.getString("details"));
                 wish.setLink(rs.getString("link"));
                 wish.setFulfilled(rs.getBoolean("fulfilled"));
                 wish.setPrice(rs.getInt("price"));
