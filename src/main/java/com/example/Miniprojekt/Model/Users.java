340c881a8f35c5b3d639876fac72d5cbf3d7a979
AmingYee <wenm0007@stud.kea.dk>
2023-03-29 10:39:21 +0200

    initial

---
+++ src/main/java/com/example/Miniprojekt/Model/Users.java
@@ -0,0 +1,26 @@
+package com.example.Miniprojekt.Model;
+
+public class Users {
+    private int id;
+    private String name;
+
+    //getter
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //setter
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/example/Miniprojekt/Model/WishList.java
@@ -0,0 +1,25 @@
+package com.example.Miniprojekt.Model;
+
+public class WishList {
+    private int id;
+    private String name;
+
+    //getter
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //setter
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
