0ee9e51e33d295339f5abd3bf4d7c191b4e9f60d
Andsemandse <113141115+Andsemandse@users.noreply.github.com>
2023-03-29 09:44:21 +0200

    SQL FIL med USER
    -

---
+++ TheWishListSQL.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS wishlist_DB;
+
+CREATE DATABASE wishlist_DB;
+USE wishlist_DB;
+
+CREATE TABLE user (
+  id INT NOT NULL AUTO_INCREMENT,
+  userName VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE wishlist (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  user_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES user(id)
+);
+
+
+CREATE TABLE wish (
+  id INT NOT NULL AUTO_INCREMENT,
+  wishName VARCHAR(255) NOT NULL,
+  details VARCHAR(255) NOT NULL,
+  price INT,
+  amount INT,
+  fulfilled TINYINT,
+  wishlist_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (wishlist_id) REFERENCES wishlist(id)
+);
+
+commit;
