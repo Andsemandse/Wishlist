8e122f5176c8180c9efff7bd8ec0a2673a7b8732
YmerA38 <stin166c@stud.kea.dk>
2023-03-29 09:57:42 +0200

    small changes

---
+++ src/main/java/com/example/Miniprojekt/Model/Wishlist.java
@@ -0,0 +1,4 @@
+package com.example.Miniprojekt.Model;
+
+public class Wishlist {
+}
--- src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
+++ src/main/java/com/example/Miniprojekt/Repository/WishListRepository.java
@@ -1,6 +1,8 @@
 package com.example.Miniprojekt.Repository;
 
+import com.example.Miniprojekt.Model.Users;
 import com.example.Miniprojekt.Model.Wish;
+import com.example.Miniprojekt.Model.Wishlist;
 import com.example.Miniprojekt.Utility.ConnectionManager;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,7 +22,46 @@ public class WishListRepository implements InterfaceRepository {
     @Value("${spring.utility.psw}")
     String psw;
 
-    // TODO: 29-03-2023 metode skal rettes.
+    public ArrayList<Users> getListOfUsers(){
+        ArrayList<Users> resultList = new ArrayList<>();
+        try {
+            Connection con = ConnectionManager.getConnection(url,user,psw);
+            String SQL = "select * from wishlist_DB.users";
+            ResultSet rs = con.createStatement().executeQuery(SQL);
+
+            while (rs.next()) {
+                Users user = new Users();
+                //user.setUserName(rs.getString("title"));
+                resultList.add(user);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException();
+        }
+
+        return resultList;
+    }
+
+
+    public ArrayList<Wishlist> getListOfWishlists(int id){
+        ArrayList<Wishlist> resultList = new ArrayList<>();
+        try {
+            Connection con = ConnectionManager.getConnection(url,user,psw);
+            String SQL = "select * from wishlist_DB.wishlist where user_id = ?";
+            PreparedStatement ps = con.prepareStatement(SQL);
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Wishlist wishlist = new Wishlist();
+                //wishlist.setTitle(rs.getString("title"));
+                resultList.add(wishlist);
+            }
+        }catch (SQLException e){
+            throw new RuntimeException();
+        }
+
+        return resultList;
+    }
     public ArrayList<Wish> getWishList(int id){
         ArrayList<Wish> resultList = new ArrayList<>();
         try {
