99522452274201f6373f1a0f2d04dad2f13c5384
AmingYee <wenm0007@stud.kea.dk>
2023-03-29 10:43:20 +0200

    wishlist

--- src/main/java/com/example/Miniprojekt/Model/Wishlist.java
+++ src/main/java/com/example/Miniprojekt/Model/Wishlist.java
@@ -1,4 +1,24 @@
 package com.example.Miniprojekt.Model;
 
 public class Wishlist {
+    private int id;
+    private String name;
+
+    //getter
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    //setter
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
